12fa00a66bb78e2aa6b98adedc8f7a48bd298406
AniDas10 <ani2004@gmail.com>
2021-02-08 01:58:29 +0530

    checkpoint 1.1 of series

--- src/create_folds.py
+++ src/create_folds.py
@@ -2,16 +2,16 @@ import pandas as pd
 from sklearn import model_selection
 
 if __name__ == "__main__":
-    df = pd.read_csv('input/train.csv')
-    df[kfold] = -1
+    df = pd.read_csv("../input/train.csv")
+    df["kfold"] = -1
 
     df = df.sample(frac=1).reset_index(drop=True)
 
     kf = model_selection.StratifiedKFold(n_splits=5,shuffle=False,random_state=None)
 
-    for fold, (train_idx, valid_idx) in enumerate(kf.split(x=df, y=df.target.values)):
+    for fold, (train_idx, valid_idx) in enumerate(kf.split(df, y=df.target.values)):
         print(len(train_idx), len(valid_idx))
         df.loc[valid_idx, 'kfold'] = fold
 
     
-    df.to_csv("input/train_folds.csv", index=False)
+    df.to_csv("../input/train_folds.csv", index=False)
